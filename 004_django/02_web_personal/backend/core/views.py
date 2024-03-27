29479d99adc28aa8e453493b77351510adbce850
NahumFG <ngutierrez2507@gmail.com>
2024-03-27 14:53:45 -0500

    inciando contenedor de react

--- 004_django/02_web_personal/backend/core/views.py
+++ 004_django/02_web_personal/backend/core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Mi web persoddsnal</h1><h2>Hola</h2>')
+    return HttpResponse('<h1>Mi web personal</h1><h2>Hola</h2>')
