c86c04a6f30ffbfc97982a57c9289ed6ada3529f
NahumFG <ngutierrez2507@gmail.com>
2024-03-29 01:31:23 -0500

    primeras urls

--- 004_django/02_web_personal/backend/core/views.py
+++ 004_django/02_web_personal/backend/core/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+html_base = """
+<h1>Mi web personal</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me/">Acerca de</a></li>
+    <li><a href="/portfolio/">Portafolio</a></li>
+    <li><a href="/contact/">Contacto</a></li>
+</ul>
+"""
+
 def home(request):
-    return HttpResponse('<h1>Mi web personal</h1><h2>Hola</h2>')
+    return HttpResponse(html_base + '<h2>Portada</h2>' + '<p>Esta es la portada</p>')
+
+def about(request):
+    return HttpResponse(html_base + '<h2>Acerca de</h2>' + '<p>Me llamo Juan y soy programador</p>')
+
+def portfolio(request):
+    return HttpResponse(html_base + '<h2>Portafolio</h2>' + '<p>Estos son mis trabajos</p>')
+
+def contact(request):
+    return HttpResponse(html_base + '<h2>Contacto</h2>' + '<p>Aquí puedes contactar conmigo</p>')
