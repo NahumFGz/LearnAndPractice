de532fa08edcbb29e701093f102629750f17cc04
NahumFG <ngutierrez2507@gmail.com>
2024-03-29 02:54:20 -0500

    templates

--- 004_django/02_web_personal/backend/core/views.py
+++ 004_django/02_web_personal/backend/core/views.py
@@ -1,25 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</a></li>
-    <li><a href="/portfolio/">Portafolio</a></li>
-    <li><a href="/contact/">Contacto</a></li>
-</ul>
-"""
-
 def home(request):
-    return HttpResponse(html_base + '<h2>Portada</h2>' + '<p>Esta es la portada</p>')
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(html_base + '<h2>Acerca de</h2>' + '<p>Me llamo Juan y soy programador</p>')
+    return render(request, "core/about.html")
 
 def portfolio(request):
-    return HttpResponse(html_base + '<h2>Portafolio</h2>' + '<p>Estos son mis trabajos</p>')
+    return render(request, "core/portfolio.html")
 
 def contact(request):
-    return HttpResponse(html_base + '<h2>Contacto</h2>' + '<p>Aquí puedes contactar conmigo</p>')
+    return render(request, "core/contact.html")
