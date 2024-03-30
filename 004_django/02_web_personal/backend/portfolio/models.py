a4b25fd527b3bf9792ec20dda86d6937cd407f30
NahumFG <ngutierrez2507@gmail.com>
2024-03-29 23:31:11 -0500

    primer proyecto terminado

--- 004_django/02_web_personal/backend/portfolio/models.py
+++ 004_django/02_web_personal/backend/portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name='Título')
     description = models.TextField(verbose_name='Descripción')
     image = models.ImageField(verbose_name='Imagen', upload_to='projects')
+    link = models.URLField(null=True, blank=True, verbose_name='Dirección web')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
     updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
 
