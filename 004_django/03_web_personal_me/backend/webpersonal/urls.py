221f450e2683d93a6fc9d42680961aae0126fd26
NahumFG <ngutierrez2507@gmail.com>
2024-03-30 02:43:41 -0500

    fin de la replica

--- 004_django/03_web_personal_me/backend/webpersonal/urls.py
+++ 004_django/03_web_personal_me/backend/webpersonal/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from core import views as core_views
 from portfolio import views as portfolio_views
 
+from django.conf import settings
+
 urlpatterns = [
     path('', core_views.home, name="home"),
     path('about/', core_views.about, name="about"),
@@ -26,3 +28,7 @@ urlpatterns = [
     path('contact/', core_views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
