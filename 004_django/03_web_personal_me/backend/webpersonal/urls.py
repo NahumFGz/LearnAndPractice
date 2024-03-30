167bb377ea714a10fc24b3f8c88e6aba14633957
NahumFG <ngutierrez2507@gmail.com>
2024-03-30 02:21:19 -0500

    antes de portaforlio

--- 004_django/03_web_personal_me/backend/webpersonal/urls.py
+++ 004_django/03_web_personal_me/backend/webpersonal/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views as core_views
+from portfolio import views as portfolio_views
 
 urlpatterns = [
+    path('', core_views.home, name="home"),
+    path('about/', core_views.about, name="about"),
+    path('portfolio/', portfolio_views.portfolio, name="portfolio"),
+    path('contact/', core_views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
