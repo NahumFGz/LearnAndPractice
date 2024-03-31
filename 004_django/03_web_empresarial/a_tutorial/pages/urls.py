c56f0c7e5ca667109fc054a8a0b58909c86f0ee2
NahumFG <ngutierrez2507@gmail.com>
2024-03-31 02:37:08 -0500

    template tags propios

--- 004_django/03_web_empresarial/a_tutorial/pages/urls.py
+++ 004_django/03_web_empresarial/a_tutorial/pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:page_id>/', views.page, name="page"),
+    path('<int:page_id>/<slug:page_slug>/', views.page, name="page"),
     path('sample/', views.sample, name="sample"),
 ]
