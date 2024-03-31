6ed424ed26ac1b38c2e19d301ea442ba0e0d574d
NahumFG <ngutierrez2507@gmail.com>
2024-03-31 02:44:21 -0500

    ckeditor

--- 004_django/03_web_empresarial/a_tutorial/pages/models.py
+++ 004_django/03_web_empresarial/a_tutorial/pages/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(max_length=200, verbose_name="titulo")
     content = models.TextField(verbose_name="contenido")
+    order = models.SmallIntegerField(default=0, verbose_name="orden")
     created = models.DateTimeField(auto_now_add=True, verbose_name="fecha de creacion")
     updated = models.DateTimeField(auto_now=True, verbose_name="fecha de edicion")
 
     class Meta:
         verbose_name = "pagina"
         verbose_name_plural = "paginas"
-        ordering = ['title']
+        ordering = ['order','title']
 
     def __str__(self):
         return self.title
