761f947c3eb15af6485b0014e342731cbe7fec5d
NahumFG <ngutierrez2507@gmail.com>
2024-04-01 00:06:13 -0500

    app social terminada

--- 004_django/03_web_empresarial/b_intento_uno/core/views.py
+++ 004_django/03_web_empresarial/b_intento_uno/core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def about(request):
+    return render(request, 'core/about.html')
+
+def blog(request):
+    return render(request, 'core/blog.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def sample(request):
+    return render(request, 'core/sample.html')
+
+def services(request):
+    return render(request, 'core/services.html')
+
+def store(request):
+    return render(request, 'core/store.html')
