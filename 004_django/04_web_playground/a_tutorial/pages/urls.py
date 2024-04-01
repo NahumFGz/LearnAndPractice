592bd98a585360876ed08cba873acd51c351ba07
NahumFG <ngutierrez2507@gmail.com>
2024-04-01 03:22:01 -0500

    fin de page list

--- 004_django/04_web_playground/a_tutorial/pages/urls.py
+++ 004_django/04_web_playground/a_tutorial/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import PageListView, PageDetailView
+from .views import PageListView, PageDetailView, PageCreateView
 
-urlpatterns = [
+pages_patterns = ([
     path('', PageListView.as_view(), name="pages"),
     path('<int:pk>/<slug:page_slug>/', PageDetailView.as_view(), name="page"),
-]
+    path('create/', PageCreateView.as_view(), name="create"),
+],'pages')
--- 004_django/04_web_playground/a_tutorial/webplayground/urls.py
+++ 004_django/04_web_playground/a_tutorial/webplayground/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pages.urls import pages_patterns
 
 urlpatterns = [
     # urls de core
     path('', include('core.urls')),
     # urls de pages
-    path('pages/', include('pages.urls')),
+    path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
 ]
