e7c78c2f58e15af557edd083e58323045433b611
NahumFG <ngutierrez2507@gmail.com>
2024-04-01 00:34:27 -0500

    agregar campos de solo lectura a service admin

--- 004_django/03_web_empresarial/b_intento_uno/services/admin.py
+++ 004_django/03_web_empresarial/b_intento_uno/services/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Service
 
 # Register your models here.
-admin.site.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    
+admin.site.register(Service, ServiceAdmin)
