b14fb1c5eb4f9b6eb05fed305b02e475fb44a6a7
NahumFG <ngutierrez2507@gmail.com>
2024-04-03 18:21:56 -0500

    componentes y estilos como funciones

--- 005_react/03_introduccion_react/c_componentes_estilo/src/App.js
+++ 005_react/03_introduccion_react/c_componentes_estilo/src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+const estilo = {
+  backgroundColor: '#456',
+  color: '#fff',
+  padding: '10px 15px',
+  margin: '10px 15px',
+}
+
+const estilo2 = {
+  boxShadow: '0 0 5px rgba(0,0,0,0.1)',
+}
+
+// Puedo pasar los parametos como valores
+const estiloFn = (bg = '#333') => (
+  {
+    backgroundColor: bg,
+    color: '#fff',
+    padding: '10px 15px',
+    margin: '10px 15px',
+  }
+)
+
+// Puedo pasar los parametos como objeto
+const estiloFn2 = ({bg = '#222'}) => (
+  {
+    backgroundColor: bg,
+    color: '#fff',
+    padding: '10px 15px',
+    margin: '10px 15px',
+  }
+)
+
+const Li = ( { children } ) => {
+  return (
+    <li style={{...estilo2, ...estiloFn(), ...estiloFn2({bg:'#333'})}} className='clase-li'>{children}</li>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul style={estiloFn('#750')} className='clase-ss'>
+      <Li estado='feliz'>Valor de li</Li>
+    </ul>
   );
 }
 
