51aa4092c11d0fc925ee926b30fc50b0446495aa
NahumFG <ngutierrez2507@gmail.com>
2024-04-04 00:19:11 -0500

    listas y keys

--- 005_react/03_introduccion_react/d_componentes_import_css/src/App.js
+++ 005_react/03_introduccion_react/d_componentes_import_css/src/App.js
@@ -1,9 +1,25 @@
 import Button from "./button";
 
+const arr = [
+  'chanchito feliz',
+  'chanchito triste',
+  'chanchito emocionado',
+]
+
 function App() {
+  const miVariableRenderizadoCondicional = false
+  
+  if (miVariableRenderizadoCondicional) {
+    return  <p>Mi variable es dio true!</p>
+  }
+
   return (
     <div>
       <h1 onClick={ (e) => console.log('clic',e)}>Hola Mundo</h1>
+      
+      //Cuando se itera un array se debe agregar un key
+      {arr.map(el => <p key={el}>{el}</p>)}
+      
       <Button onClick={() => console.log('clikeado')}> Enviar </Button>
     </div>
   )
