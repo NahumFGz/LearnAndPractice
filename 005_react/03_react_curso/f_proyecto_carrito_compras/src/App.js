fe0a748dae26d759dc47e42e72042cc4e1e2000d
NahumFG <ngutierrez2507@gmail.com>
2024-04-04 04:35:59 -0500

    botton de agregar al carro

---
+++ 005_react/03_react_curso/f_proyecto_carrito_compras/src/App.js
@@ -0,0 +1,25 @@
+import { Component } from "react";
+import Productos from "./components/Productos";
+
+class App extends Component {
+  state = {
+    productos: [
+      { name: "Tomate", price: 1500, img: "/productos/tomate.jpg" },
+      { name: "Arbejas", price: 2500, img: "/productos/arbejas.jpg" },
+      { name: "Lechuga", price: 500, img: "/productos/lechuga.jpg" },
+    ],
+  };
+
+  render() {
+    return (
+      <div>
+        <Productos
+          agregarAlCarro={() => console.log("No hace nada")}
+          productos={this.state.productos}
+        />
+      </div>
+    )
+  }
+}
+
+export default App;
