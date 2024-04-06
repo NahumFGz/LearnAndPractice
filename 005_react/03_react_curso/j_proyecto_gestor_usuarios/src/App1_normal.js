3739d548cbcb8a58ae49aac77bc11027e5b00d8c
NahumFG <ngutierrez2507@gmail.com>
2024-04-06 17:01:14 -0500

    proyecto formularios

---
+++ 005_react/03_react_curso/j_proyecto_gestor_usuarios/src/App1_normal.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import useFormulario from "./hooks/useFormulario";
+import Input from "./components/Input";
+import Card from "./components/Card";
+import Container from "./components/Container";
+import Button from "./components/Button";
+
+function App() {
+  const [usuarios, setUsuarios] = useState([]);
+  const [formulario, handleChange, reset] = useFormulario({
+    name: "",
+    lastname: "",
+    email: "",
+  });
+
+  const submit = (e) => {
+    e.preventDefault();
+    setUsuarios([...usuarios, formulario]);
+
+    reset();
+  };
+
+  console.log(formulario);
+  console.log(formulario, usuarios);
+  return (
+    <div style={{ marginTop: '15px'}}>
+      <Container>
+        <Card>
+          <div style={{ padding: 20 }}>
+            <form onSubmit={submit}>
+              <Input
+                label="Nombre"
+                name="name"
+                value={formulario.name}
+                onChange={handleChange}
+                placeholder='Nombre'
+              />
+              <Input
+                label="Apellido"
+                name="lastname"
+                value={formulario.lastname}
+                onChange={handleChange}
+                placeholder='Apellido'
+              />
+              <Input
+                label="Correo"
+                name="email"
+                value={formulario.email}
+                onChange={handleChange}
+                placeholder='Email'
+              />
+              <Button>Enviar</Button>
+            </form>
+          </div>
+        </Card>
+        <Card>
+          <ul>
+            {usuarios.map((x) => (
+              <li key={x.email}>{`${x.name} ${x.lastname}: ${x.email}`}</li>
+            ))}
+          </ul>
+        </Card>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ 005_react/03_react_curso/j_proyecto_gestor_usuarios/src/components/UserForm.js
@@ -0,0 +1,47 @@
+import Input from "./Input";
+import Button from "./Button";
+import useFormulario from "../hooks/useFormulario";
+
+
+const UserForm = ({ submit }) => {
+    const [formulario, handleChange, reset] = useFormulario({
+        name: "",
+        lastname: "",
+        email: "",
+      });
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        submit(formulario)
+        reset()
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+              <Input
+                label="Nombre"
+                name="name"
+                value={formulario.name}
+                onChange={handleChange}
+                placeholder='Nombre'
+              />
+              <Input
+                label="Apellido"
+                name="lastname"
+                value={formulario.lastname}
+                onChange={handleChange}
+                placeholder='Apellido'
+              />
+              <Input
+                label="Correo"
+                name="email"
+                value={formulario.email}
+                onChange={handleChange}
+                placeholder='Email'
+              />
+              <Button>Enviar</Button>
+            </form>
+    )
+}
+
+export default UserForm
