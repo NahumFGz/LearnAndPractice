e4fbd9359cd752c6046754e6e3d6974540ec043a
NahumFG <ngutierrez2507@gmail.com>
2024-04-07 18:13:33 -0500

    finalizando tutoriales

--- 008_fullstack_projects/01_task_app/backend/tasks/models.py
+++ 008_fullstack_projects/01_task_app/backend/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200, verbose_name='titulo')
+    description = models.TextField(blank=True)
+    done = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'proyecto'
+        verbose_name_plural = 'proyectos'
+        ordering = ['proyecto']
+
+    def __str__(self) -> str:
+        return self.title
