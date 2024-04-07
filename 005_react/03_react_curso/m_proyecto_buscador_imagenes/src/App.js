94ac7e2ad786c78dd11b02dd7159fce5f89bd8d8
NahumFG <ngutierrez2507@gmail.com>
2024-04-07 02:49:54 -0500

    proyecto buscador terminado

--- 005_react/03_react_curso/m_proyecto_buscador_imagenes/src/App.js
+++ 005_react/03_react_curso/m_proyecto_buscador_imagenes/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Formik, Form, Field } from 'formik'
+import './header.css'
+import './content.css'
+import './article.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+  const [photos, setPhotos] = useState([])
+
+  const open = (url) => window.open(url)
+
+  console.log('Photos: ',{ photos })
+
+  return(
+    <div>
+      <header>
+        <Formik
+          initialValues={{search: ''}}
+          onSubmit={async values => {
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20?&query=${values.search}`, {
+              headers: {
+                'Authorization': 'Client-ID QCeqhwvn85RFYNF5dTnzTUysLGusL1JjloasQ-0zrS0' 
+              }
+            })
+            const data = await response.json()
+          // Actualizar el estado
+          setPhotos(data.results)
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search"/>
+          </Form>
+
+        </Formik>
       </header>
+
+      <div className='container'>
+          <div className='center'>
+            {photos.map(photo => <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} alt='x'/>
+              <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+            </article>)}
+          </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
