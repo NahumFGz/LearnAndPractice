dddba74572aee01fae65cf4a5e3acc9c2320077d
NahumFG <ngutierrez2507@gmail.com>
2024-04-06 20:38:31 -0500

    replica de formulario

--- 005_react/03_react_curso/k_proyecto_replica/src/App.js
+++ 005_react/03_react_curso/k_proyecto_replica/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import useFormulario from './hooks/useFormulario'
+import Input from './components/Input'
+import Button from './components/Button'
+import Card from './components/Card'
+import Container from './components/Container'
 
 function App() {
+  const [usuarios, setUsuarios] = useState([])
+  const [formulario, handleChange, reset] = useFormulario({
+    name: '',
+    lastname: '',
+    email: '',
+  })
+
+  const submit = (e) => {
+    e.preventDefault();
+    setUsuarios([...usuarios, formulario]);
+
+    reset()
+  }
+  
+
+  console.log(formulario)
+  console.log(formulario, usuarios)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{marginTop:'15px'}}>
+      <Container>
+        <Card>
+          <div style={{padding:20}}>
+            <form onSubmit={submit}>
+              <Input 
+                label='Nombre'
+                name='name'
+                value={formulario.name}
+                onChange={handleChange}
+                placeholder='Nombre'
+              />
+              <Input 
+                label='Apellido'
+                name='lastname'
+                value={formulario.lastname}
+                onChange={handleChange}
+                placeholder='Apellido'
+              />
+              <Input 
+                label='Correo'
+                name='email'
+                value={formulario.email}
+                onChange={handleChange}
+                placeholder='Email'
+              />
+              <Button>Enviar</Button>
+            </form>
+          </div>
+        </Card>
+        <Card>
+          <ul>
+            {usuarios.map((x) => (
+              <li key={x.email}>{`${x.name} ${x.lastname}: ${x.email}`}</li>
+            ))}
+          </ul>
+        </Card>
+      </Container>
     </div>
   );
 }
