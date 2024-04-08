2614cacd3f7ddc9ed90d94cb88de1ac07f18626a
NahumFG <ngutierrez2507@gmail.com>
2024-04-08 02:25:59 -0500

    crear tareas

--- 008_fullstack_projects/01_task_app/frontend/task-app/src/api/tasks.api.js
+++ 008_fullstack_projects/01_task_app/frontend/task-app/src/api/tasks.api.js
@@ -1,5 +1,14 @@
 import axios from 'axios'
 
-export const getAllTasks = () => {
-    return axios.get('http://localhost:8000/tasks/api/v1/tasks/')
-}
+const taskApi = axios.create({
+    baseURL: 'http://localhost:8000/tasks/api/v1/tasks/'
+})
+
+// export const getAllTasks = () => {   
+    // return axios.get('http://localhost:8000/tasks/api/v1/tasks/')
+//     return taskApi.get('/')
+// }
+
+export const getAllTasks = () => taskApi.get('/')
+
+export const createTask = (task) => taskApi.post('/', task)
--- 008_fullstack_projects/01_task_app/frontend/task-app/src/pages/TaskFormPage.jsx
+++ 008_fullstack_projects/01_task_app/frontend/task-app/src/pages/TaskFormPage.jsx
@@ -1,5 +1,36 @@
-export function TaskFormPage(){
+import { useForm } from 'react-hook-form'
+import { createTask } from '../api/tasks.api'
+import { useNavigate } from 'react-router-dom'
+
+export function TaskFormPage() {
+
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const navigate = useNavigate()
+
+    const onSubmit = handleSubmit(async data => {
+        console.log(data)
+        const response = await createTask(data)
+        console.log('response: ', response)
+
+        //Una vez se termine de crear se debe navegar al listado de tasks
+        navigate('/tasks')
+    })
+
     return (
-        <div>Task Form Page</div>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="title"
+                    {...register("title", { required: true })}
+                />
+                {errors.title && <span>this field is required</span>}
+
+                <textarea rows="3" placeholder="description"
+                    {...register("description", { required: true })}
+                />
+                {errors.description && <span>this field is required</span>}
+                <button>Save</button>
+            </form>
+        </div>
     )
 }
