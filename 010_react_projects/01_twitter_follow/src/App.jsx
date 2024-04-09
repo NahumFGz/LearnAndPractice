5a35d9c4f57cb27faa64ec8af6a6f3f944ac40eb
NahumFG <ngutierrez2507@gmail.com>
2024-04-09 10:23:05 -0500

    elementos css a nivel de html

---
+++ 010_react_projects/01_twitter_follow/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+export function App() {
+    return (
+        <article>
+            <header>
+                <img alt="El avatar de midudev" src="https://unavatar.io/midudev" />
+                <div>
+                    <strong>Miguel Ángel Durám</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
