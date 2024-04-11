4b334d9c68507b4e9dee4f2990c43c564aca154f
NahumFG <ngutierrez2507@gmail.com>
2024-04-10 22:31:56 -0500

    toggle mounted

--- 010_react_projects/03_mouse_follower/src/App.jsx
+++ 010_react_projects/03_mouse_follower/src/App.jsx
@@ -1,19 +1,66 @@
 import { useEffect, useState } from 'react'
 
-function App () {
-  const [enabled, setEnabled] = useState(true)
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
   useEffect(() => {
-    console.log('Efecto')
-  })
+    console.log('Efecto', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // Se va limpiar la suscripción en las consola se puede ver con getEventListeners(window)
+    // -> Se ejecuta cuando el componente se desmonta
+    // -> Cuando cambian las dependencias
+    // -> Antes de ejecutar el efecto de nuevo
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
     <>
-      <h3>Proyecto 3</h3>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Desactivar' : 'Activar'} seguir puntero
       </button>
     </>
+  )
+}
+
+function App () {
+  const [mounted, setMounted] = useState(true)
+
+  return (
+    <main>
+      {mounted && <FollowMouse />}
+      <br />
+      <button onClick={() => setMounted(!mounted)}>
+        Toggle mounted FollowMouse component
+      </button>
+    </main>
   )
 }
 
