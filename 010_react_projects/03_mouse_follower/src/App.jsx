ca376d363067a51c2c0931bc23ef94f4a8b85637
NahumFG <ngutierrez2507@gmail.com>
2024-04-10 21:38:28 -0500

    creando estados

--- 010_react_projects/03_mouse_follower/src/App.jsx
+++ 010_react_projects/03_mouse_follower/src/App.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from 'react'
+
 function App () {
+  const [enabled, setEnabled] = useState(true)
+
+  useEffect(() => {
+    console.log('Efecto')
+  })
+
   return (
-    <h3>Proyecto 3</h3>
+    <>
+      <h3>Proyecto 3</h3>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </>
   )
 }
 
