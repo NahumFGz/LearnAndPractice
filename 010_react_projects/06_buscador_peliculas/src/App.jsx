c45479521e46383d3b16c090c009d3ddee1b088a
NahumFG <ngutierrez2507@gmail.com>
2024-04-14 13:43:48 -0500

    creacion de hooks

--- 010_react_projects/06_buscador_peliculas/src/App.jsx
+++ 010_react_projects/06_buscador_peliculas/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useMovies } from './hooks/useMovies'
+import { Movies } from './components/Movies'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  const { movies } = useMovies()
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='page'>
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form className='form'>
+          <input placeholder='Avengers, StarWars, The Matrix ....' />
+          <button type='submit'> Buscar </button>
+        </form>
+      </header>
+
+      <main>
+        <Movies movies={movies} />
+      </main>
+    </div>
   )
 }
 
---
+++ 010_react_projects/06_buscador_peliculas/src/components/Movies.jsx
@@ -0,0 +1,33 @@
+export function ListOfMovies ({ movies }) {
+  return (
+    <ul>
+      {
+        movies.map(movie =>
+          (
+            <li key={movie.id}>
+              <h3>{movie.title}</h3>
+              <p>{movie.year}</p>
+              <img src={movie.poster} alt={movie.Title} />
+            </li>
+          ))
+      }
+    </ul>
+  )
+}
+
+export function NoMoviesResults () {
+  return (
+    <p>No se encontarron resultados</p>
+  )
+}
+
+export function Movies ({ movies }) {
+  const hasMovies = movies?.length > 0
+
+  return (
+    hasMovies
+      ? <ListOfMovies movies={movies} />
+      : <NoMoviesResults />
+
+  )
+}
