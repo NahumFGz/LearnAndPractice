8605cc079027d531625e3e582479448e75df4600
NahumFG <ngutierrez2507@gmail.com>
2024-04-18 23:01:16 -0500

    limpiando carrito

--- 010_react_projects/07_carrito_compras/src/components/Footer.jsx
+++ 010_react_projects/07_carrito_compras/src/components/Footer.jsx
@@ -1,25 +1,9 @@
 import './Footer.css'
-import { useFilters } from '../hooks/useFilters'
-import { useCart } from '../hooks/useCart'
-
 export function Footer () {
-  const { filters } = useFilters()
-  const { cart } = useCart()
-
   return (
     <footer className='footer'>
-      {
-        JSON.stringify(filters, null, 2)
-      }
-      {
-        JSON.stringify(cart, null, 2)
-      }
-      {
-        /*
-        <h4>Prueba técnica de React ⚛️ － <span>@midudev</span></h4>
-        <h5>Shopping Cart con useContext & useReducer</h5>
-        */
-      }
+      <h4>Prueba técnica de React ⚛️ － <span>@midudev</span></h4>
+      <h5>Shopping Cart con useContext & useReducer</h5>
     </footer>
   )
 }
