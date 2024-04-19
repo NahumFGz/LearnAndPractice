dc1022089ed1b0b25b8d0277b7b0def2b8caa77b
NahumFG <ngutierrez2507@gmail.com>
2024-04-18 20:59:10 -0500

    filtros desordenados

--- 010_react_projects/07_carrito_compras/src/App.jsx
+++ 010_react_projects/07_carrito_compras/src/App.jsx
@@ -1,10 +1,11 @@
-import { products as initialProducts, products } from './mocks/products.json'
+import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products'
 import { useState } from 'react'
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+import { IS_DEVELOPMENT } from './config'
 
-function App () {
-  const [products, setProducts] = useState(initialProducts)
+function useFilters () {
   const [filters, setFilters] = useState({
     category: 'all',
     minPrice: 0
@@ -22,11 +23,20 @@ function App () {
     })
   }
 
+  return { filterProducts, filters, setFilters }
+}
+
+function App () {
+  const [products] = useState(initialProducts)
+  const { filterProducts, filters, setFilters } = useFilters()
+
   const filteredProducts = filterProducts(products)
+
   return (
     <>
       <Header changeFilters={setFilters} />
       <Products products={filteredProducts} />
+      {IS_DEVELOPMENT && <Footer filters={filters} />}
     </>
   )
 }
