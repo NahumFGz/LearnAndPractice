17fe3c8cad0ae5c6482225dfa65073bb60dbe167
NahumFG <ngutierrez2507@gmail.com>
2024-04-18 23:11:56 -0500

    carrito terminado

--- 010_react_projects/07_carrito_compras/src/components/Cart.jsx
+++ 010_react_projects/07_carrito_compras/src/components/Cart.jsx
@@ -1,9 +1,32 @@
 import './Cart.css'
 import { useId } from 'react'
 import { CartIcon, ClearCartIcon } from './Icons.jsx'
+import { useCart } from '../hooks/useCart.js'
+
+function CartItem ({ thumbnail, price, title, quantity, addToCart }) {
+  return (
+    <li>
+      <img
+        src={thumbnail}
+        alt={title}
+      />
+      <div>
+        <strong> {title} </strong> - ${price}
+      </div>
+
+      <footer>
+        <small>
+          Qty: {quantity}
+        </small>
+        <button onClick={addToCart}>+</button>
+      </footer>
+    </li>
+  )
+}
 
 export function Cart () {
   const cartCheckboxId = useId()
+  const { cart, clearCart, addToCart } = useCart()
 
   return (
     <>
@@ -14,25 +37,15 @@ export function Cart () {
 
       <aside className='cart'>
         <ul>
-          <li>
-            <img
-              src='https://cdn.dummyjson.com/product-images/1/thumbnail.jpg'
-              alt='Iphone'
+          {cart.map(product => (
+            <CartItem
+              key={product.id}
+              addToCart={() => addToCart(product)}
+              {...product}
             />
-            <div>
-              <strong> iPhone </strong> - $1499
-            </div>
-
-            <footer>
-              <small>
-                Qty: 1
-              </small>
-              <button>+</button>
-            </footer>
-          </li>
+          ))}
         </ul>
-
-        <button>
+        <button onClick={clearCart}>
           <ClearCartIcon />
         </button>
       </aside>
