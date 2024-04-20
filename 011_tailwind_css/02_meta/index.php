4f0e46ee831c70deb203559f5286e74446c6049d
NahumFG <ngutierrez2507@gmail.com>
2024-04-20 01:18:43 -0500

    correcciones

---
+++ 011_tailwind_css/02_meta/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Meta: compra gafas inteligentes y de VR</title>
+
+  <!-- Tailwind CSS -->
+  <script src="https://cdn.tailwindcss.com"></script>
+
+  <!-- Alpine JS -->
+  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</head>
+
+<body>
+
+  <?php require_once 'secciones/top-bar.php'; ?>
+
+</body>
+
+</html>
---
+++ 011_tailwind_css/02_meta/secciones/top-bar.php
@@ -0,0 +1,21 @@
+<div class="py-3 flex justify-center gap-10 text-xs text-white bg-[#344854]">
+   <div>Envío gratis</div>
+   <div class="flex gap-2 items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+         class="w-5 h-5">
+         <path stroke-linecap="round" stroke-linejoin="round"
+            d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+      </svg>
+      Pruebas sin compromiso
+   </div>
+
+   <div class='flex gap-2'>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+         class="w-6 h-6">
+         <path stroke-linecap="round" stroke-linejoin="round"
+            d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+      </svg>
+
+      Garantíaa
+   </div>
+</div>
