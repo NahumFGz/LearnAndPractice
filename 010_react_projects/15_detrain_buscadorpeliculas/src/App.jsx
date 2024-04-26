6748b25c6a3f02ba51cebac635f046ee46db2b7d
NahumFG <ngutierrez2507@gmail.com>
2024-04-26 18:33:24 -0500

    fin de maquetado

--- 010_react_projects/15_detrain_buscadorpeliculas/src/App.jsx
+++ 010_react_projects/15_detrain_buscadorpeliculas/src/App.jsx
@@ -1,4 +1,8 @@
+import responseMovies from './mocks/with-results.json'
+
 function App () {
+  const movies = responseMovies.Search
+
   return (
     <>
       <div className='flex flex-col items-center'>
@@ -12,14 +16,25 @@ function App () {
             placeholder='Avengers, StarWars, The Matrix...'
           />
           <input className='m-2' type='checkbox' />
-          <button className='w-24 h-10 border text-white text-lg rounded-lg bg-slate-700 hover:bg-slate-500'> Buscar </button>
+          <button className={`w-24 h-10 border text-white text-lg rounded-lg bg-slate-700 
+                            hover:bg-slate-500
+                            active:scale-95 transition duration-150 ease-in-out
+                            `}
+          > Buscar
+          </button>
         </form>
       </div>
 
       <main>
-        <div>
+        <div className='text-white font-bold grid grid-cols-3 gap-4 mt-10'>
           {
-
+              movies.map((movie) => (
+                <div key={movie.imdbID} className='flex flex-col items-center'>
+                  <img src={movie.Poster} alt={movie.Title} />
+                  <h2 className=''>{movie.Title}</h2>
+                  <p className=''>{movie.Year}</p>
+                </div>
+              ))
           }
         </div>
       </main>
