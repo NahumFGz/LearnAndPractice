ca82608208414ac3a7987951979a43dbdb8c66eb
NahumFG <ngutierrez2507@gmail.com>
2024-04-25 21:54:53 -0500

    base terminada

--- 010_react_projects/14_detrain_pruebatecnica/src/App.jsx
+++ 010_react_projects/14_detrain_pruebatecnica/src/App.jsx
@@ -1,7 +1,22 @@
 function App () {
   return (
     <>
-      <p className='text-white text-3xl'>Prueba técnica</p>
+      <h1 className=' py-10 text-white text-3xl flex justify-center'>Prueba técnica</h1>
+
+      <article className='text-base text-white flex items-center justify-center gap-10'>
+        <section>
+          <p className='ml-10'>
+            Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum
+          </p>
+        </section>
+
+        <aside>
+          <img
+            className='max-w-[300px] m-10'
+            src='https://i.natgeofe.com/k/ad9b542e-c4a0-4d0b-9147-da17121b4c98/MOmeow1_square.png'
+          />
+        </aside>
+      </article>
     </>
   )
 }
