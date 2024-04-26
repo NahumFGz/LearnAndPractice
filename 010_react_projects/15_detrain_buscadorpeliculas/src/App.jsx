ba662bd07b4f9826afadacba17e77a7c92a62406
NahumFG <ngutierrez2507@gmail.com>
2024-04-26 18:13:34 -0500

    header completado

--- 010_react_projects/15_detrain_buscadorpeliculas/src/App.jsx
+++ 010_react_projects/15_detrain_buscadorpeliculas/src/App.jsx
@@ -1,9 +1,29 @@
 function App () {
   return (
     <>
-      <h1 className='text-3xl underline text-white'>
-        Hello world!
-      </h1>
+      <div className='flex flex-col items-center'>
+        <h1 className='py-10 text-3xl text-white font-bold'>
+          Buscador de peliculas
+        </h1>
+
+        <form>
+          <input
+            className='w-92 h-10 px-4 text-lg rounded-lg'
+            placeholder='Avengers, StarWars, The Matrix...'
+          />
+          <input className='m-2' type='checkbox' />
+          <button className='w-24 h-10 border text-white text-lg rounded-lg bg-slate-700 hover:bg-slate-500'> Buscar </button>
+        </form>
+      </div>
+
+      <main>
+        <div>
+          {
+
+          }
+        </div>
+      </main>
+
     </>
   )
 }
