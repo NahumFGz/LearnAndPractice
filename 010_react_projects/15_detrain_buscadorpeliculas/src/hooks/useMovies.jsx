96d02d496455adfb8c54024835c004d0e7fbaecf
NahumFG <ngutierrez2507@gmail.com>
2024-04-27 18:56:06 -0500

    use movies

--- 010_react_projects/15_detrain_buscadorpeliculas/src/hooks/useMovies.jsx
+++ 010_react_projects/15_detrain_buscadorpeliculas/src/hooks/useMovies.jsx
@@ -1,25 +1,13 @@
-import withResults from '../mocks/with-results.json'
-import withoutResults from '../mocks/no-results.json'
+import { searchMovies } from '../services/movies'
 import { useState } from 'react'
 
 export function useMovies ({ search }) {
-  const [responseMovies, setResponseMovies] = useState([])
-  const movies = responseMovies.Search
+  const [movies, setMovies] = useState([])
 
-  const mappedMovies = movies?.map((movie) => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster
-  }))
-
-  const getMovies = () => {
-    if (search) {
-      setResponseMovies(withResults)
-    } else {
-      setResponseMovies(withoutResults)
-    }
+  const getMovies = async () => {
+    const newMovies = await searchMovies({ search })
+    setMovies(newMovies)
   }
 
-  return { movies: mappedMovies, getMovies }
+  return { movies, getMovies }
 }
---
+++ 010_react_projects/15_detrain_buscadorpeliculas/src/hooks/useMovies1.jsx
@@ -0,0 +1,24 @@
+import { searchMovies } from '../services/movies'
+import { useState } from 'react'
+
+export function useMovies ({ search }) {
+  const [responseMovies, setResponseMovies] = useState([])
+  const movies = responseMovies.Search
+
+  const mappedMovies = movies?.map((movie) => ({
+    id: movie.imdbID,
+    title: movie.Title,
+    year: movie.Year,
+    poster: movie.Poster
+  }))
+
+  const getMovies = () => {
+    if (search) {
+      setResponseMovies(withResults)
+    } else {
+      setResponseMovies(withoutResults)
+    }
+  }
+
+  return { movies: mappedMovies, getMovies }
+}
