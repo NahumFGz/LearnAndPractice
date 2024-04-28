69539a344ca8e748cf4d5b508fd7311f40ac870e
NahumFG <ngutierrez2507@gmail.com>
2024-04-28 15:30:10 -0500

    busqueda y validaciones

--- 010_react_projects/17_detrain_buscadorpeliculas/src/App.jsx
+++ 010_react_projects/17_detrain_buscadorpeliculas/src/App.jsx
@@ -1,21 +1,67 @@
+import { useState, useEffect } from 'react'
 import { Movies } from './components/Movies'
 import mockResults from './mocks/with-results.json'
 
 function App () {
   const movies = mockResults?.Search
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('handleSubmit', { query })
+  }
+
+  const handleTypeSearch = (event) => {
+    const newQuery = event.target.value
+    setQuery(newQuery)
+    console.log('handleTypeSearch', { newQuery })
+  }
+
+  const handleSort = (event) => {
+    const isChecked = event.target.checked
+    console.log('handleSort', { isChecked })
+  }
+
+  useEffect(() => {
+    // Validaciones de query
+    if (query === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (query.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    if (query.match(/^\d+$/)) {
+      setError('No se puede buscar una pelicula con un número')
+      return
+    }
+
+    setError(null)
+  }, [query])
 
   return (
     <>
       <div className='bg-gray-900 p-4'>
         <h1 className='text-white text-2xl font-bold mb-4'> Buscador de peliculas </h1>
 
-        <form className='flex flex-wrap gap-2'>
+        <form
+          className='flex flex-wrap gap-2'
+          onSubmit={handleSubmit}
+        >
           <input
+            name='query'
             className='flex-1 p-2'
             placeholder='Avengers, Star Wars, The Matrix...'
+            onChange={handleTypeSearch}
           />
           <input
+            name='check'
             type='checkbox'
+            onChange={handleSort}
           />
 
           <button
@@ -26,6 +72,7 @@ function App () {
             Button text
           </button>
         </form>
+        {error && <p className='mt-4 text-red-500 text-sm'>{error}</p>}
       </div>
 
       <main className='bg-gray-800 p-4'>
