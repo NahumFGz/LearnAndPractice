06153d6ca25cdfa50e894dd406e974b82f3a82e7
NahumFG <ngutierrez2507@gmail.com>
2024-04-28 16:48:12 -0500

    busqueda repetida

--- 010_react_projects/17_detrain_buscadorpeliculas/src/hooks/useMovies.jsx
+++ 010_react_projects/17_detrain_buscadorpeliculas/src/hooks/useMovies.jsx
@@ -1,16 +1,20 @@
 import { searchMovies } from '../services/movies'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 export function useMovies ({ search }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [errorMovies, setErrorMovies] = useState(null)
+  const previousSearch = useRef('')
 
   const getMovies = async () => {
+    if (search === previousSearch.current) return console.log('Busqueda repetida')
+
     try {
       setLoading(true)
       setErrorMovies(null)
 
+      previousSearch.current = search
       const newMovies = await searchMovies({ search })
       setMovies(newMovies)
     } catch (e) {
