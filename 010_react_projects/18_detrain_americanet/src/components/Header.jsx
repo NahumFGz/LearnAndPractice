449bf5ba35b820ae68272e782fd0a8985a48b9da
NahumFG <ngutierrez2507@gmail.com>
2024-04-29 15:10:17 -0500

    Header Routes complete

--- 010_react_projects/18_detrain_americanet/src/components/Header.jsx
+++ 010_react_projects/18_detrain_americanet/src/components/Header.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { ROUTES } from '../config/routes'
+import { Link } from 'react-router-dom'
 
 const navigation = [
   { name: 'Inicio', href: ROUTES.HOME },
@@ -18,10 +19,10 @@ export default function Header () {
     <header className='bg-white'>
       <nav className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8' aria-label='Global'>
         <div className='flex lg:flex-1'>
-          <a href='#' className='-m-1.5 p-1.5'>
+          <Link to={ROUTES.HOME} className='-m-1.5 p-1.5'>
             <span className='sr-only'>Your Company</span>
             <img className='h-8 w-auto' src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600' alt='' />
-          </a>
+          </Link>
         </div>
         <div className='flex lg:hidden'>
           <button
@@ -35,29 +36,29 @@ export default function Header () {
         </div>
         <div className='hidden lg:flex lg:gap-x-12'>
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className='text-sm font-semibold leading-6 text-gray-900'>
+            <Link key={item.name} to={item.href} className='text-sm font-semibold leading-6 text-gray-900'>
               {item.name}
-            </a>
+            </Link>
           ))}
         </div>
         <div className='hidden lg:flex lg:flex-1 lg:justify-end'>
-          <a href='#' className='text-sm font-semibold leading-6 text-gray-900'>
+          <Link to={ROUTES.LOGIN} className='text-sm font-semibold leading-6 text-gray-900'>
             Log in <span aria-hidden='true'>&rarr;</span>
-          </a>
+          </Link>
         </div>
       </nav>
       <Dialog as='div' className='lg:hidden' open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className='fixed inset-0 z-10' />
         <Dialog.Panel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
           <div className='flex items-center justify-between'>
-            <a href='#' className='-m-1.5 p-1.5'>
+            <Link to={ROUTES.HOME} className='-m-1.5 p-1.5'>
               <span className='sr-only'>Your Company</span>
               <img
                 className='h-8 w-auto'
                 src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
                 alt=''
               />
-            </a>
+            </Link>
             <button
               type='button'
               className='-m-2.5 rounded-md p-2.5 text-gray-700'
@@ -71,22 +72,22 @@ export default function Header () {
             <div className='-my-6 divide-y divide-gray-500/10'>
               <div className='space-y-2 py-6'>
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
-                    href={item.href}
+                    to={item.href}
                     className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <div className='py-6'>
-                <a
-                  href='#'
+                <Link
+                  to={ROUTES.LOGIN}
                   className='-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                 >
                   Log in
-                </a>
+                </Link>
               </div>
             </div>
           </div>
