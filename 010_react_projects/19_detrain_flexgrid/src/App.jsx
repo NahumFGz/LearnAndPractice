201fc332555154a72f7ea2ee13a184637be7d9ba
NahumFG <ngutierrez2507@gmail.com>
2024-05-01 16:24:59 -0500

    style: :art: Practica de Flex y grid

--- 010_react_projects/19_detrain_flexgrid/src/App.jsx
+++ 010_react_projects/19_detrain_flexgrid/src/App.jsx
@@ -1,8 +1,17 @@
 function App () {
   return (
-    <p>
-      Hola Mundo
-    </p>
+
+    <div className='m-[200px] border py-10'>
+      {/* //? flex basis */}
+      <div className='flex flex-row gap-2'>
+        <div className='basis-1/2 border rounded p-2 w-24'>01</div>
+        <div className='basis-1/4 border rounded p-2 w-24'>02</div>
+        <div className='basis-1/4 border rounded p-2 w-24'>03</div>
+      </div>
+
+      {/* //? flex direction */}
+
+    </div>
   )
 }
 
