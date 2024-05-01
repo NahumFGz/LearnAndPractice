d47cecf4ee3bbeab0c7e42942b0c444a94cef481
NahumFG <ngutierrez2507@gmail.com>
2024-05-01 17:30:20 -0500

    flex order

--- 010_react_projects/19_detrain_flexgrid/src/App.jsx
+++ 010_react_projects/19_detrain_flexgrid/src/App.jsx
@@ -1,28 +1,33 @@
 // import { FlexBasisWrap } from './components/FlexBasisWrap'
 // import { FlexGrowShrink } from './components/FlexGrowShrink'
+import { FlexOrder } from './components/FlexOrder'
 
 function App () {
   return (
     // <FlexBasisWrap />
     // <FlexGrowShrink />
-    <div className='mt-40 mx-80 border py-10'>
-      {/* //? flex order */}
-      <p>flex order</p>
-      <div className='flex flex-row justify-between'>
-        <div className='border rounded p-2 w-24'>01</div>
-        <div className='border rounded p-2 w-24'>02</div>
-        <div className='border rounded p-2 w-24'>03</div>
-      </div>
+    <FlexOrder />
 
-      {/* //? flex -order */}
-      <p className='mt-[20px]'>flex -order</p>
-      <div className='flex flex-row justify-between'>
-        <div className='border rounded p-2 w-24'>01</div>
-        <div className='border rounded p-2 w-24'>02</div>
-        <div className='border rounded p-2 w-24'>03</div>
-      </div>
+  // <div className='mt-40 mx-80 border py-10'>
+  //   {/* //? flex order */}
+  //   <p>flex order</p>
+  //   <div className='flex flex-row justify-between'>
+  //     <div className='order-last border rounded p-2 w-24'>01</div>
+  //     <div className='border rounded p-2 w-24'>02</div>
+  //     <div className='border rounded p-2 w-24'>03</div>
+  //     <div className='border rounded p-2 w-24'>04</div>
+  //   </div>
 
-    </div>
+  //   {/* //? flex -order */}
+  //   <p className='mt-[20px]'>flex -order</p>
+  //   <div className='flex flex-row justify-between'>
+  //     <div className='order-1 border rounded p-2 w-24'>01</div>
+  //     <div className='border rounded p-2 w-24'>02</div>
+  //     <div className='border rounded p-2 w-24'>03</div>
+  //     <div className='border rounded p-2 w-24'>04</div>
+  //   </div>
+
+  // </div>
 
   )
 }
