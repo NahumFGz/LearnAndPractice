2d0df3c7453968b3f20fd9ed664934a9681fe829
NahumFG <ngutierrez2507@gmail.com>
2024-05-02 18:59:40 -0500

    use reducer

---
+++ 010_react_projects/16_detrain_carrito_compras/src/context/cart2.jsx
@@ -0,0 +1,46 @@
+import { createContext, useState } from 'react'
+
+// Creear el contexto
+export const CartContext = createContext()
+
+// Crear el provider
+export function CartProvider ({ children }) {
+  const [cart, setCart] = useState([])
+
+  const addToCart = (product) => {
+    // Forma sencilla
+    // setCart([...cart, product])
+
+    // Check if product is already in cart
+    const productCartIndex = cart.findIndex(item => item.id === product.id)
+
+    if (productCartIndex >= 0) {
+      // Una forma seria usando structuredClone
+      const newCart = structuredClone(cart)
+      newCart[productCartIndex].quantity++
+      return setCart(newCart)
+    }
+
+    // Producto no está en el carrito
+    setCart(prevState => ([...prevState, { ...product, quantity: 1 }]))
+  }
+
+  const removeFromCart = product => {
+    setCart(prevState => prevState.filter(item => item.id !== product.id))
+  }
+  const clearCart = () => {
+    setCart([])
+  }
+
+  return (
+    <CartContext.Provider value={{
+      cart,
+      addToCart,
+      removeFromCart,
+      clearCart
+    }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
