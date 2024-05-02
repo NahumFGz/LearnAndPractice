9836bdbc6be9f03e2bf149d274cca4c35747a9ca
NahumFG <ngutierrez2507@gmail.com>
2024-05-02 13:36:09 -0500

    feat: :construction: Inicialización de lectura de productos

--- 010_react_projects/16_detrain_carrito_compras/src/App.jsx
+++ 010_react_projects/16_detrain_carrito_compras/src/App.jsx
@@ -1,7 +1,11 @@
+import { products as initialProducts } from './mocks/products.json'
+import { Products } from './components/Products'
+
 function App () {
   return (
     <div className='text-white'>
       <p>Carrito de compras C: </p>
+      <Products products={initialProducts} />
     </div>
   )
 }
---
+++ 010_react_projects/16_detrain_carrito_compras/src/components/Products.jsx
@@ -0,0 +1,26 @@
+import { AddToCartIcon } from './Icons'
+
+export function Products ({ products }) {
+  return (
+    <main>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+            />
+            <div>
+              <strong>{product.title}</strong>
+            </div>
+            <div>
+              <button>
+                <AddToCartIcon />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
