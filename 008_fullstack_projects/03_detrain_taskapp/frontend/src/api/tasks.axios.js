2dc7bb2fd938e6b740a3a232c01c36170b7381d0
NahumFG <ngutierrez2507@gmail.com>
2024-05-03 23:53:11 -0500

    formularios completados

--- 008_fullstack_projects/03_detrain_taskapp/frontend/src/api/tasks.axios.js
+++ 008_fullstack_projects/03_detrain_taskapp/frontend/src/api/tasks.axios.js
@@ -6,6 +6,10 @@ const taskApi = axios.create({
 
 export const getAllTasks = () => taskApi.get('/')
 
+export const getTask = (id) => taskApi.get(`/${id}/`)
+
 export const createTask = (task) => taskApi.post('/', task)
 
 export const deleteTask = (id) => taskApi.delete(`/${id}/`)
+
+export const updateTask = (id, task) => taskApi.put(`/${id}/`, task)
--- 008_fullstack_projects/03_detrain_taskapp/frontend/src/pages/TasksFormPage.jsx
+++ 008_fullstack_projects/03_detrain_taskapp/frontend/src/pages/TasksFormPage.jsx
@@ -1,20 +1,41 @@
 import { useForm } from 'react-hook-form'
-import { createTask, deleteTask } from '../api/tasks.axios'
+import { createTask, deleteTask, updateTask, getTask } from '../api/tasks.axios'
 import { useNavigate, useParams } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export function TasksFormPage () {
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const { register, handleSubmit, formState: { errors }, setValue } = useForm()
   const navigate = useNavigate()
   const params = useParams()
   console.log('params -> ', params)
 
   const handleFormSubmit = handleSubmit(async data => {
-    const res = await createTask(data)
-    console.log(res)
+    if (params.id) {
+      await updateTask(params.id, data)
+      console.log('update task')
+    } else {
+      await createTask(data)
+      console.log('Create task')
+    }
+    navigate('/tasks')
   })
 
+  useEffect(() => {
+    if (params.id) {
+      async function loadTask () {
+        console.log('load task')
+        const response = await getTask(params.id)
+        console.log('response -> ', response)
+        setValue('title', response.data.title)
+        setValue('description', response.data.description)
+      }
+
+      loadTask()
+    }
+  }, [])
+
   return (
-    <div className='border w-96 h-[350px] rounded-md p-2 flex items-center justify-center'>
+    <div className='border w-96 h-[350px] rounded-md p-2 flex flex-col gap-4 items-center justify-center'>
       <form onSubmit={handleFormSubmit}>
         <div className='flex flex-col gap-4 mt-3'>
           <input
@@ -42,34 +63,34 @@ export function TasksFormPage () {
               </button>
             )
           }
+        </div>
+      </form>
 
-          {params.id && (
-            <button
-              className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'
-              onClick={async () => {
-                const accept = window.confirm('Are you sure you want to delete this task?')
-                if (accept) {
-                  await deleteTask(params.id)
-                  navigate('/tasks')
-                }
-              }}
-            >
-              Delete
-            </button>
-          )}
+      {params.id && (
+        <button
+          className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'
+          onClick={async () => {
+            const accept = window.confirm('Are you sure you want to delete this task?')
+            if (accept) {
+              await deleteTask(params.id)
+              navigate('/tasks')
+            }
+          }}
+        >
+          Delete
+        </button>
+      )}
 
-          {
+      {
             params.id && (
               <button
                 className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded'
+                onClick={handleFormSubmit}
               >
                 Update
               </button>
             )
           }
-
-        </div>
-      </form>
     </div>
   )
 }
