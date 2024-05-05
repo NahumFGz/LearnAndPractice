162f4423adf3622f77f00cbba25b9b0d99c1f44d
NahumFG <ngutierrez2507@gmail.com>
2024-05-05 01:07:35 -0500

    elimnar por grupo e insertar por grupo

--- 008_fullstack_projects/04_detrain_taskapp/backend/tasks/serializer.py
+++ 008_fullstack_projects/04_detrain_taskapp/backend/tasks/serializer.py
@@ -3,7 +3,25 @@ from rest_framework import serializers
 from .models import Task
 
 
+# Definir el TaskSerializer principal
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = "__all__"
+
+
+# Definir el ListSerializer utilizando TaskSerializer definido anteriormente
+class TaskBulkCreateSerializer(serializers.ListSerializer):
+    child = TaskSerializer()
+
+    def create(self, validated_data):
+        tasks = [Task(**item) for item in validated_data]
+        return Task.objects.bulk_create(tasks)
+
+
+# Re-define TaskSerializer para especificar el list_serializer_class
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = "__all__"
+        list_serializer_class = TaskBulkCreateSerializer
--- 008_fullstack_projects/04_detrain_taskapp/backend/tasks/views.py
+++ 008_fullstack_projects/04_detrain_taskapp/backend/tasks/views.py
@@ -1,10 +1,30 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Task
-from .serializer import TaskSerializer
+from .serializer import TaskBulkCreateSerializer, TaskSerializer
 
 
 # Create your views here.
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+    @action(detail=False, methods=["post"], url_path="bulk-create")
+    def bulk_create(self, request, *args, **kwargs):
+        serializer = TaskBulkCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=["delete"], url_path="delete-all")
+    def delete_all(self, request, *args, **kwargs):
+        # Eliminar todas las tareas
+        count = Task.objects.all().delete()
+        return Response(
+            {"message": f"Successfully deleted {count[0]} tasks."},
+            status=status.HTTP_204_NO_CONTENT,
+        )
