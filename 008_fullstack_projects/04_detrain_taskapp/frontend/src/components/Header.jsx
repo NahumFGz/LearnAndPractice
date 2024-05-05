c56012a99dcc5a942ae5d92c1605a421b9385522
NahumFG <ngutierrez2507@gmail.com>
2024-05-04 19:18:03 -0500

    toogle buttons

--- 008_fullstack_projects/04_detrain_taskapp/frontend/src/components/Header.jsx
+++ 008_fullstack_projects/04_detrain_taskapp/frontend/src/components/Header.jsx
@@ -1,6 +1,9 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export function Header () {
+  const location = useLocation()
+  const isInTasksPage = location.pathname === '/tasks'
+
   return (
     <header className='flex flex-row items-center justify-between'>
       <Link to='/tasks'>
@@ -11,17 +14,23 @@ export function Header () {
       </Link>
 
       <div>
-        <Link to='/tasks-create'>
-          <button className='border white rounded-sm m-2 p-1 text-white'>
-            Insert Task
-          </button>
-        </Link>
-        <button className='border white rounded-sm m-2 p-1 text-white'>
-          Delete All
-        </button>
-        <button className='border white rounded-sm m-2 p-1 text-white'>
-          Insert Sample
-        </button>
+        {
+            isInTasksPage && (
+              <div>
+                <Link to='/tasks-create'>
+                  <button className='border white rounded-sm m-2 p-1 text-white'>
+                    Insert Task
+                  </button>
+                </Link>
+                <button className='border white rounded-sm m-2 p-1 text-white'>
+                  Delete All
+                </button>
+                <button className='border white rounded-sm m-2 p-1 text-white'>
+                  Insert Sample
+                </button>
+              </div>
+            )
+        }
       </div>
 
     </header>
