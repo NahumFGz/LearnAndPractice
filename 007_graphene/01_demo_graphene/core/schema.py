dd869ff2dc1080f62842a47638f38ce4f411cc24
NahumFG <ngutierrez2507@gmail.com>
2024-05-13 12:46:09 -0500

    buscar por id

--- 007_graphene/01_demo_graphene/core/schema.py
+++ 007_graphene/01_demo_graphene/core/schema.py
@@ -7,15 +7,19 @@ from books.models import Book
 class BookType(DjangoObjectType):
     class Meta:
         model = Book
-        fields = ("id", "title", "description")
+        fields = ("id", "title", "description", "created_at", "updated_at")
 
 
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hi!")
+    book = graphene.Field(BookType, id=graphene.ID())
     books = graphene.List(BookType)
 
     def resolve_books(self, info):
         return Book.objects.all()
+
+    def resolve_book(self, info, id):
+        return Book.objects.get(pk=id)
 
 
 schema = graphene.Schema(query=Query)
