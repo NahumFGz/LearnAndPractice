4742073fe3560a4915080740d5f6a2537029ffc7
NahumFG <ngutierrez2507@gmail.com>
2024-05-13 12:31:50 -0500

    QueryBooks

--- 007_graphene/01_demo_graphene/core/schema.py
+++ 007_graphene/01_demo_graphene/core/schema.py
@@ -1,8 +1,21 @@
 import graphene
+from graphene_django.types import DjangoObjectType
+
+from books.models import Book
+
+
+class BookType(DjangoObjectType):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "description")
 
 
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hi!")
+    books = graphene.List(BookType)
+
+    def resolve_books(self, info):
+        return Book.objects.all()
 
 
 schema = graphene.Schema(query=Query)
